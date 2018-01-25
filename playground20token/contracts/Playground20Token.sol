df1b1a7e4cc9d555073a58c654243d32fb6cf767
piyodev <dev@piyolab.com>
2018-01-25 09:16:03 +0900

    fix issues

--- playground20token/contracts/Playground20Token.sol
+++ playground20token/contracts/Playground20Token.sol
@@ -1,17 +1,17 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/MintableToken.sol";
 
 contract Playground20Token is MintableToken {
 
-    string const public name = "Playground20Token";   // トークンの名称
-    string public symbol = "PG20";                    // トークンのシンボル
-    uint8 public decimals = 18;                       // トークンの小数点以下の桁数
+    string public constant name = "Playground20Token";   // トークンの名称
+    string public constant symbol = "PG20";              // トークンのシンボル
+    uint8 public constant decimals = 18;                 // トークンの小数点以下の桁数
 
     // コンストラクタ
     // このコンストラクタでは何もせずに、
     // トークンを初期化する親のスマートコントラクトで、トークンの初期発行等を行う。
-    function Playground20Token() {
+    function Playground20Token() public {
     }
 
 }
--- playground20token/contracts/Playground20TokenGenerator.sol
+++ playground20token/contracts/Playground20TokenGenerator.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
-import 'zeppelin-solidity/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import './Playground20Token.sol';
 
 contract Playground20TokenGenerator {
@@ -11,18 +11,18 @@ contract Playground20TokenGenerator {
 	Playground20Token public token;
 
 	// Ether とトークンのベース交換レート
-	uint256 constant public rate;		// todo レートは固定じゃないほうが楽しいかも
+	uint256 public constant baseRate = 100;
 
 	// トークン発行数のキャップ
-	uint256 constant public cap;		// todo 21000000 * 10*18 
+	uint256 public constant cap = 21000000 * 10*18; 
 
 	// トークン発行時に送付された Ether を受け取る wallet
-	address constant wallet = "0xac82f34a1a287B2206982A9220e7b68846C645dF";
+	address internal constant wallet = 0xac82f34a1a287B2206982A9220e7b68846C645dF;
 	
  	event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
 	// コンストラクタ
-	function Playground20TokenGenerator() {
+	function Playground20TokenGenerator() public {
 		token = new Playground20Token();
 	}
 
@@ -34,11 +34,15 @@ contract Playground20TokenGenerator {
 		require(beneficiary != address(0));
 		require(msg.value != 0);
 	    uint256 weiAmount = msg.value;
-        uint256 tokenAmount = weiAmount.mul(rate);
+        uint256 tokenAmount = weiAmount.mul(getRate());
         // todo capを確認する
         token.mint(beneficiary, tokenAmount);
         TokenPurchase(msg.sender, beneficiary, weiAmount, tokenAmount);
         wallet.transfer(msg.value);
 	}
-	
+
+	function getRate() internal pure returns (uint256) {
+		return baseRate.mul(10);
+	}
+
 }
