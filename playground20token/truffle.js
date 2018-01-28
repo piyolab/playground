b55b7ee11c7f9cb0b3672667581f72c4525b1447
piyodev <dev@piyolab.com>
2018-01-28 13:00:02 +0900

    update deploy script

--- playground20token/truffle.js
+++ playground20token/truffle.js
@@ -1,4 +1,22 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNEMONIC;
+var infuraAccessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+  	// テストネット用デプロイ設定
+  	ropsten: {
+  		provider: function() {
+        	return new HDWalletProvider(
+        		mnemonic,
+          		"https://ropsten.infura.io/" + infuraAccessToken
+        	);
+      	},
+      	network_id: 3,
+    	gas: 4000000,
+    	gasPrice: 21000000000	// 21 Gwei
+  	}
+  }
 };
