d4b0b1964643b206887d6b85108fabefd19cad29
piyodev <dev@piyolab.com>
2018-01-30 02:15:46 +0900

    add a local testnet deploy setting

--- playground20token/truffle.js
+++ playground20token/truffle.js
@@ -6,15 +6,23 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-  	// テストネット用デプロイ設定
+    // Local geth 用デプロイ設定
+    local: {
+      network_id: 8888,
+      host: "localhost",
+      port: 8888,
+      gas: 4000000,
+      gasPrice: 21000000000 // 21 Gwei
+    },
+    // Ropsten テストネット用デプロイ設定
   	ropsten: {
   		provider: function() {
         	return new HDWalletProvider(
         		mnemonic,
           		"https://ropsten.infura.io/" + infuraAccessToken
         	);
-      	},
-      	network_id: 3,
+      },
+      network_id: 3,
     	gas: 4000000,
     	gasPrice: 21000000000	// 21 Gwei
   	}
