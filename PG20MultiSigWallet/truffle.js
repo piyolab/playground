7aa05622692ac5a8582e0ad6663ca10871d5b624
piyodev <dev@piyolab.com>
2018-02-23 08:00:55 +0900

    update truffle.js

--- PG20MultiSigWallet/truffle.js
+++ PG20MultiSigWallet/truffle.js
@@ -1,4 +1,30 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNEMONIC;
+var infuraAccessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    // Local geth 用デプロイ設定
+    local: {
+      network_id: 8888,
+      host: "localhost",
+      port: 8888,
+      gas: 4000000,
+      gasPrice: 21000000000 // 21 Gwei
+    },
+    // Ropsten テストネット用デプロイ設定
+  	ropsten: {
+  		provider: function() {
+        	return new HDWalletProvider(
+        		mnemonic,
+          		"https://ropsten.infura.io/" + infuraAccessToken
+        	);
+      },
+      network_id: 3,
+    	gas: 4000000,
+    	gasPrice: 21000000000	// 21 Gwei
+  	}
+  }
 };
