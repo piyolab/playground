d52504004c8d40a78b65e53ee87f4cbc18f12ab5
piyodev <dev@piyolab.com>
2018-02-23 09:44:25 +0900

    update

--- PG20MultiSigWallet/public/js/multisig.js
+++ PG20MultiSigWallet/public/js/multisig.js
@@ -1,8 +1,13 @@
-var INFURA_API_KEY = "";
-var HTTP_PROVIDER_URL = "https://ropsten.infura.io/";
+const INFURA_API_KEY = '';
+const HTTP_PROVIDER_URL = 'https://ropsten.infura.io/';
 
-var DEFAULT_MULTI_SIG_WALLET_ADDRESS = "0x1370f0273a409a01b36989b8c5ff8c3e42989e14";
+const DEFAULT_MULTI_SIG_WALLET_ADDRESS = '0x1370f0273a409a01b36989b8c5ff8c3e42989e14';
+const DEFAULT_MULTI_SIG_WALLET_ABI = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"owners","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"}],"name":"removeOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"transactionId","type":"uint256"}],"name":"revokeConfirmation","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"},{"name":"","type":"address"}],"name":"confirmations","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"pending","type":"bool"},{"name":"executed","type":"bool"}],"name":"getTransactionCount","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"}],"name":"addOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"transactionId","type":"uint256"}],"name":"isConfirmed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"transactionId","type":"uint256"}],"name":"getConfirmationCount","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"transactions","outputs":[{"name":"destination","type":"address"},{"name":"value","type":"uint256"},{"name":"data","type":"bytes"},{"name":"executed","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getOwners","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"from","type":"uint256"},{"name":"to","type":"uint256"},{"name":"pending","type":"bool"},{"name":"executed","type":"bool"}],"name":"getTransactionIds","outputs":[{"name":"_transactionIds","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"transactionId","type":"uint256"}],"name":"getConfirmations","outputs":[{"name":"_confirmations","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"transactionCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_required","type":"uint256"}],"name":"changeRequirement","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"transactionId","type":"uint256"}],"name":"confirmTransaction","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"destination","type":"address"},{"name":"value","type":"uint256"},{"name":"data","type":"bytes"}],"name":"submitTransaction","outputs":[{"name":"transactionId","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_OWNER_COUNT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"required","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"},{"name":"newOwner","type":"address"}],"name":"replaceOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"transactionId","type":"uint256"}],"name":"executeTransaction","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_owners","type":"address[]"},{"name":"_required","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":true,"name":"transactionId","type":"uint256"}],"name":"Confirmation","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":true,"name":"transactionId","type":"uint256"}],"name":"Revocation","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"transactionId","type":"uint256"}],"name":"Submission","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"transactionId","type":"uint256"}],"name":"Execution","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"transactionId","type":"uint256"}],"name":"ExecutionFailure","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"}],"name":"OwnerAddition","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"}],"name":"OwnerRemoval","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"required","type":"uint256"}],"name":"RequirementChange","type":"event"}];
 
+const wallet = {};
+
+
+// URL パラメータを取得	
 function getUrlParams() {
 	var params  = new Object;
 	const url = location.search.substring(1).split('&');
@@ -13,13 +18,54 @@ function getUrlParams() {
 	return params;
 }
 
-$(document).ready(function(){
+
+// Wallet 情報を初期化
+function initWallet() {
 	const params = getUrlParams();
+
 	if (params.address) {
-		$('#address').val(params.address);
+		wallet.address = params.address;
 	} else {
-		$('#address').val(DEFAULT_MULTI_SIG_WALLET_ADDRESS);
+		wallet.address = DEFAULT_MULTI_SIG_WALLET_ADDRESS;
 	}
+
+	if(params.abi) {
+		wallet.abi = params.abi;
+	} else {
+		wallet.abi = DEFAULT_MULTI_SIG_WALLET_ABI;
+	}
+
+	wallet.contract = window.web3.eth.contract(wallet.abi).at(wallet.address);
+}
+
+
+// Wallet 情報を表示
+function displayWalletInfo() {
+	$('#address').val(wallet.address);
+	$('#abi').val(JSON.stringify(wallet.abi));
+
+	// MultiSigWallet の Owner アドレスを取得
+	(function(method) { wallet.contract[method](function(error, result){
+	    $('#owners').val(result);
+	});})('getOwners');
+
+
+}
+
+
+$(document).ready(function(){
+	
+	if (typeof web3 !== 'undefined') {
+    	// Use Mist/MetaMask's provider
+    	console.log('Using MetaMask!')
+	    window.web3 = new Web3(web3.currentProvider);
+	} else {
+		window.web3 = new Web3(new Web3.providers.HttpProvider(HTTP_PROVIDER_URL + INFURA_API_KEY));
+	}
+
+	initWallet();
+	displayWalletInfo();
+
 });
 
 
